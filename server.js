e42132bd4737d5e1c9f3120bed33cc747d49867f
Kang Kujun <donato14@naver.com>
2023-02-24 00:44:25 +0900

    Add nodemon

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.listen(8080, function () {
   console.log('listening on 8080');
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
 app.get('/pet', function (req, res) {
   res.send('펫 용품을 쇼핑할 수 있는 페이지입니다.');
 });
@@ -12,3 +16,4 @@ app.get('/pet', function (req, res) {
 app.get('/beauty', function (req, res) {
   res.send('뷰티용품을 쇼핑할 수 있는 페이지입니다.');
 });
+
