3cf258576da7e5e95330d42cb735904ba85dc1c9
Kang Kujun <donato14@naver.com>
2023-03-05 01:43:31 +0900

    수정요청 method-override패키지

--- server.js
+++ server.js
@@ -98,3 +98,48 @@ app.put('/edit', function (req, res) {
     res.redirect('/list')
   })
 })
+
+
+const passport = require('passport');
+const LocalStratgy = require('passport-local');
+const session = require('express-session');
+const { Passport } = require('passport');
+
+app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function (req, res) {
+  res.render('login.ejs');
+});
+app.post('/login', passport.authenticate('local', {
+  failureRedirect : '/fail'
+}), function(req, res) {
+  res.redirect('/')
+});
+
+passport.use(new LocalStratgy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+});
+passport.deserializeUser(function (아이디, done) {
+  done(null, {})
+});
